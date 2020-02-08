f694b0cdd493a22b75b25782708469a99fa7e7d5
Jacob Byers <byers.jacob@gmail.com>
2020-02-07 17:20:11 -0700

    Fixed config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,11 +8,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-netlify-cms`,
     {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `markdown-pages`,
-      path: `${__dirname}/blog`,
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/blog`,
+      },
     },
     `gatsby-transformer-remark`,
-  },]
+  ]
 }
